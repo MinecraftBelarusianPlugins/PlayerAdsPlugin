ca3bf0ad55c5776ed17233019a28001de9badffd
Siarhiej Bahdaniec <sweid531@gmail.com>
2023-10-03 01:10:03 +0400

    Impl messages

--- src/main/java/by/siarhiejbahdaniec/playeradsplugin/command/AdCommandExecutor.java
+++ src/main/java/by/siarhiejbahdaniec/playeradsplugin/command/AdCommandExecutor.java
@@ -28,6 +28,10 @@ public class AdCommandExecutor implements CommandExecutor {
     @NotNull
     private final TimeFormatter timeFormatter;
 
+    private final static String OPERATOR_MESSAGE_PREFIX = ChatColor.GOLD +
+            ChatColor.ITALIC.toString() +
+            "[PlayerAdsPlugin] ";
+
     public AdCommandExecutor(@NotNull ConfigHolder configHolder,
                              @NotNull LastAdTimeRepo lastAdTimeRepo,
                              @NotNull TimeFormatter timeFormatter) {
@@ -124,9 +128,7 @@ public class AdCommandExecutor implements CommandExecutor {
 
         if (sender.isOp() && args[0].equals("reload")) {
             configHolder.reloadConfigFromDisk();
-            sender.sendMessage(ChatColor.GOLD +
-                    ChatColor.BOLD.toString() +
-                    "[PlayerAdsPlugin] " +
+            sender.sendMessage(OPERATOR_MESSAGE_PREFIX +
                     configHolder.getString(ConfigKeys.Resources.configReloaded));
             return true;
         }
@@ -139,8 +141,16 @@ public class AdCommandExecutor implements CommandExecutor {
 
         if (sender.isOp() && args[0].equals("reset")) {
             var playerName = args[1];
-            lastAdTimeRepo.setLastAdTime(playerName, 0L);
-            sender.sendMessage("Player's timer was reset!");
+            var player = Bukkit.getServer().getPlayer(playerName);
+            if (player != null) {
+                lastAdTimeRepo.setLastAdTime(playerName, 0L);
+                sender.sendMessage(OPERATOR_MESSAGE_PREFIX +
+                        configHolder.getString(ConfigKeys.Resources.playerTimerReset)
+                                .formatted(playerName));
+            } else {
+                sender.sendMessage(OPERATOR_MESSAGE_PREFIX +
+                        configHolder.getString(ConfigKeys.Resources.playerNotFound));
+            }
             return true;
         }
 
